4d892eb7237b6cca58b0181947588f4381e398bc
Ivan Hu <ivan.hu@canonical.com>
2012-06-19 11:19:48 +0800

    tests: Add a test to check invalid PKCS7 signature attaching
    Add a test for the invalid PKCS7 signature attaching. This test
    generates 1K of zero bytes as an invalid signature to attach.
    
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>
    Signed-off-by: Jeremy Kerr <jeremy.kerr@canonical.com>

---
+++ tests/sign-invalidattach-verify.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+. "$srcdir/common.sh"
+
+invsig="test.invsig"
+dd if=/dev/zero of="$invsig" bs=1 count=1k
+tmp_image=test.pecoff
+cp "$image" "$tmp_image"
+
+set +e
+"$sbattach" --attach "$invsig" "$tmp_image"
+rc=$?
+set -e
+
+test $rc -eq 1
